b1560c38844453d1a7f2d80358bbc26f08a76757
rileylnapier <riley@symphonycommerce.com>
2016-03-30 16:19:49 -0700

    remove dev-test

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -34,25 +34,6 @@ gulp.task('dev', () => {
   );
 });
 
-gulp.task('dev-test', () => {
-  return runSequence(
-    'build-clean',
-    'tsd',
-    'compile',
-    'test',
-    'watch-ts',
-    'start-test' 
-  )
-});
-
-gulp.task('start-test', () => {
-  const devConfig = require('./dev-config.json');
-  return nodemon({
-    script: `./${APP}/app.js`,
-    env: devConfig,
-    tasks: ['test']
-  });
-});
 
 gulp.task('test', function () {
   var folder = (argv.folder === undefined) ? '**' : argv.folder;
