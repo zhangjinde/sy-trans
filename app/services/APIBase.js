0948a9e4a01a2c97e4f2844c5324f1bb67d0ca15
ryan-symphony <ryan@symphonycommerce.com>
2016-05-25 17:41:24 -0700

    app

--- app/services/APIBase.js
+++ app/services/APIBase.js
@@ -5,50 +5,37 @@
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var Service_Base_1 = require('./../bases/Service-Base');
 var btoa2 = require("btoa");
 
-var APIBase = function (_Service_Base_1$defau) {
-    _inherits(APIBase, _Service_Base_1$defau);
+var APIBase = function APIBase(apiOptions) {
+    _classCallCheck(this, APIBase);
 
-    function APIBase(apiOptions) {
-        _classCallCheck(this, APIBase);
-
-        var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(APIBase).call(this));
-
-        var manageEnv = apiOptions.env || "manage";
-        _this.headers = {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        };
-        _this.logger = apiOptions.logger ? apiOptions.logger : console;
-        _this.apiPath = apiOptions.apiPath;
-        _this.baseUrl = "https://" + manageEnv + ".symphonycommerce.com";
-        if (apiOptions.basicAuth) {
-            _this.headers["Authorization"] = apiOptions.basicAuth;
-            return _possibleConstructorReturn(_this);
-        }
-        if (apiOptions.email && apiOptions.password) {
-            _this.headers["Authorization"] = "Basic " + btoa2(apiOptions.email + ":" + apiOptions.password);
-            return _possibleConstructorReturn(_this);
-        }
-        if (apiOptions.sessionId) {
-            _this.headers["Cookie"] = "SPSESSIONID=" + apiOptions.sessionId;
-            return _possibleConstructorReturn(_this);
-        }
-        if (apiOptions.cookieAuth) {
-            _this.headers["Cookie"] = apiOptions.cookieAuth;
-            return _possibleConstructorReturn(_this);
-        }
-        return _this;
+    // super();
+    var manageEnv = apiOptions.env || "manage";
+    this.headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    };
+    this.logger = apiOptions.logger ? apiOptions.logger : console;
+    this.apiPath = apiOptions.apiPath;
+    this.baseUrl = "https://" + manageEnv + ".symphonycommerce.com";
+    if (apiOptions.basicAuth) {
+        this.headers["Authorization"] = apiOptions.basicAuth;
+        return;
     }
-
-    return APIBase;
-}(Service_Base_1.default);
+    if (apiOptions.email && apiOptions.password) {
+        this.headers["Authorization"] = "Basic " + btoa2(apiOptions.email + ":" + apiOptions.password);
+        return;
+    }
+    if (apiOptions.sessionId) {
+        this.headers["Cookie"] = "SPSESSIONID=" + apiOptions.sessionId;
+        return;
+    }
+    if (apiOptions.cookieAuth) {
+        this.headers["Cookie"] = apiOptions.cookieAuth;
+        return;
+    }
+};
 
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = APIBase;
