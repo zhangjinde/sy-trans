8098ea258dcbed599998f7d46e355257575ac762
ryan-symphony <ryan@symphonycommerce.com>
2016-07-22 18:51:35 -0700

    merge and create new version

--- test/ftp.spec.js
+++ test/ftp.spec.js
@@ -1,6 +1,6 @@
-import sinon from "sinon";
-import sinonChai from "sinon-chai";
-import chai, { expect } from "chai";
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import chai, { expect } from 'chai';
 
 const should = chai.should();
 const _ = require('lodash');
@@ -10,29 +10,41 @@ import FTPService from '../app/services/FTP';
 const sandbox = sinon.sandbox.create();
 
 const mockOptions = {
-    username: "fakeuser"
+    username: 'fakeuser'
 }
 
-const mockList = [{
-    file: "first"
-},{
-    file: "second"
-}]
+const mockList = [
+    {
+        file: 'first'
+    },
+    {
+        file: 'second'
+    }
+]
+
+const mockFile = 'File contents.'
 
 function mockFtp() {
     return Promise.resolve({
         list: (path, callback) => {
             if (!path) {
-                callback("missing path");
+                callback('missing directory path');
             }
 
             callback(null, mockList);
+        },
+        file: (file, callback) => {
+            if (!file.path) {
+                callback('missing file path');
+            }
+
+            callback(null, mockFile);
         }
     });
 }
 
 chai.use(sinonChai);
-sandbox.stub(FTPService.prototype, "initFtp").returns(mockFtp());
+sandbox.stub(FTPService.prototype, 'initFTP').returns(mockFtp());
 
 describe('FTP Service', () => {
     let ftpService;
@@ -46,9 +58,28 @@ describe('FTP Service', () => {
     });
 
     it('should return a list', (done) => {    
-        ftpService.readDir("some path").then((list) => {
+        ftpService.readDir('path/to/directory').then((list) => {
             expect(list).to.deep.equal(mockList);
             done();
         });
     });
+
+    it('should return a file object', (done) => {
+        ftpService.readFile({ content: null, path: 'path/to/file' }).then((contents) => {
+            expect(contents).to.deep.equal(mockFile);
+            done();
+        });
+    }); 
 });
+
+
+
+
+
+
+
+
+
+
+
+
